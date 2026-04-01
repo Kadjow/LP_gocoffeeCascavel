f36bd35bf0351b1e34c26580847cf794ac0e5420
Diogo Gulhak <dgulhak@gmail.com>
2026-04-01 10:24:22 -0300

    feat : added video and a hero to organize it

--- src/app/features/landing/components/hero/hero.component.ts
+++ src/app/features/landing/components/hero/hero.component.ts
@@ -1,6 +1,5 @@
-import { ChangeDetectionStrategy, Component, computed, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { BusinessModel } from '../../../../core/models/business.model';
-import { ProductHighlight } from '../../../../core/models/product-highlight.model';
 
 @Component({
   selector: 'app-hero',
@@ -10,8 +9,4 @@ import { ProductHighlight } from '../../../../core/models/product-highlight.mode
 })
 export class HeroComponent {
   readonly business = input.required<BusinessModel>();
-  readonly products = input.required<ProductHighlight[]>();
-  readonly featuredProducts = computed(() => this.products().slice(0, 2));
-  readonly heroImageSrc = '/images/hero-main-user-art.png';
-  readonly heroImageAlt = 'Ilustracao artistica colorida com formas florais e organicas em estilo pintado.';
 }
